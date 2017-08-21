5a8969aa0ad1781313a94960395bf8949c2336ab
yayayouyou66 <1141867573@qq.com>
2017-08-21 14:56:50 +0800

    实体类

--- Model/Entity/ScaffoldPara.cs
+++ Model/Entity/ScaffoldPara.cs
@@ -5,7 +5,29 @@ using System.Text;
 
 namespace Model.Entity
 {
-    class ScaffoldPara
-    {
+    public static class ScaffoldPara
+    { 
+        public static string Pro_Id { get; set; }
+        public static string Sca_Type { get; set; }
+        public static int Con_Layers { get; set; }
+        public static int Act_Layers{ get; set; }
+        public static string Base_Type{ get; set; }
+        public static string Soil_Types{ get; set; }
+        public static string Rough_Level{ get; set; }
+        public static string Cha_Value{ get; set; }
+        public static string Pad_Area{ get; set; }
+        public static string Anchor_Style{ get; set; }
+        public static string Anchor_Type{ get; set; }
+        public static string Anchor_Model{ get; set; }
+        public static string Anchor_Connect{ get; set; }
+        public static string Sca_Situation{ get; set; }
+        public static string Bui_Status{ get; set; }
+        public static string Bui_Distance { get; set; }
+        public static int  Per_Brace{ get; set; }
+        public static int Per_Level{ get; set; }
+        public static int Per_Set{ get; set; }
+        public static double La { get; set; }
+        public static double Lb { get; set; }
+        public static double H { get; set; }
     }
 }
