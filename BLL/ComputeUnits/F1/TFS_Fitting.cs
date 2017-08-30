1368a8453ff8a679bd1ba3f1c1fb386830b7bd86
Solodye <xiblade@163.com>
2017-08-30 15:08:39 +0800

    根据材料查表的封�

--- BLL/ComputeUnits/F1/TFS_Fitting.cs
+++ BLL/ComputeUnits/F1/TFS_Fitting.cs
@@ -2,10 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using DAL;
+
 
 namespace BLL.ComputeUnits.F1
 {
-    class TFS_Fitting
+    public class TFS_Fitting : Table<DataTable>
     {
+        double la;
+        double lb;
+        double h;
+        string fitting_model;
+        
+        public TFS_Fitting(double la ,double lb ,double h ,string fitting_model)
+        {
+            this.la = la;
+            this.lb = lb;
+            this.h = h;
+            this.fitting_model = fitting_model;
+        }
+        public override DataTable Search()
+        {
+            DataTable dt = new CheckTableHandle().SearchFitting(la, lb, h, fitting_model);
+            if (dt.Rows.Count == 5)
+            {
+                _isSearched = true;
+                _targetValue = dt;
+                return _targetValue ;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 函数按照对应表中的单位返回值
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public double targetValueSearch(string name,string target)
+        {
+            if (_isSearched)
+            {
+                for (int i = 0; i < _targetValue.Rows.Count; i++)
+                {
+                    if (_targetValue.Rows[i]["name"].ToString().Equals(name))
+                    {
+                        return (double)_targetValue.Rows[i][target];
+                    }
+                }
+            }
+            return -1;
+        }
     }
 }
