eba1c2fc01412d0d9700549a613ae86656810221
Solodye <xiblade@163.com>
2017-08-30 15:11:34 +0800

    添加了TFS_Fitting的注释

--- BLL/ComputeUnits/F1/TFS_Fitting.cs
+++ BLL/ComputeUnits/F1/TFS_Fitting.cs
@@ -15,6 +15,7 @@ namespace BLL.ComputeUnits.F1
         double h;
         string fitting_model;
         
+        //接收数据的单位：毫米
         public TFS_Fitting(double la ,double lb ,double h ,string fitting_model)
         {
             this.la = la;
@@ -22,6 +23,7 @@ namespace BLL.ComputeUnits.F1
             this.h = h;
             this.fitting_model = fitting_model;
         }
+        //返回单位：为规范里A-1和C-2直接能查到的单位
         public override DataTable Search()
         {
             DataTable dt = new CheckTableHandle().SearchFitting(la, lb, h, fitting_model);
