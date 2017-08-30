035323c84b87c35d4e2fa88f613ba0d7383b7333
LOL2016Vista <516112085@qq.com>
2017-08-30 15:16:28 +0800

    lmd and ng1k

--- BLL/ComputeUnits/F1/F_Lmd.cs
+++ BLL/ComputeUnits/F1/F_Lmd.cs
@@ -5,12 +5,62 @@ using System.Text;
 
 namespace BLL.ComputeUnits.F1
 {
-    public class F_Lmd: Formula<double>
+    public class F_Lmd: Formula<int>
     {
+        //立杆长度系数
+        private double μ;
+        //步距 cm
+        private double h;
+        //回转半径 cm
+        private double i;
 
-        public override double ComputeValue()
+        private double Lmd;
+
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        /// <param name="u">cm</param>
+        /// <param name="h">cm</param>
+        /// <param name="i">cm</param>
+        public F_Lmd(double u,double h, double i) 
         {
-            throw new NotImplementedException();
+            this.μ = u;
+            this.h = h;
+            this.i = i;
         }
+
+        /// <summary>
+        /// 重写ComputeValue方法，返回int型
+        /// </summary>
+        /// <returns></returns>
+        public override int ComputeValue()
+        {
+            //计算lmd的值
+            Lmd = (μ * h) / i;
+            //将lmd的计算值上取整，并给属性赋值
+            _targetValue = (int)Math.Ceiling(Lmd);
+            //计算标志位
+            _isComputed = true;
+            //返回该计算单元的目标值
+            return _targetValue;         
+        }
+
+        /// <summary>
+        /// 重写ToString方法
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (_isComputed)
+            {
+                return "(" + μ.ToString("#0.0") + "*" + h.ToString("#0.0") + ")/" + i.ToString("#0.0") + "=" + _targetValue.ToString();
+            }
+            else 
+            {
+                return "";
+            }
+        }
+
+
     }
 }
--- BLL/ComputeUnits/F1/F_NG1K.cs
+++ BLL/ComputeUnits/F1/F_NG1K.cs
@@ -5,7 +5,59 @@ using System.Text;
 
 namespace BLL.ComputeUnits.F1
 {
-    class F_NG1K
+    public class F_NG1K:Formula<double>
     {
+        //立杆自重
+        private double GStandingTube;
+        //纵向水平杆自重
+        private double GVLedger;
+        //横向水平杆自重
+        private double GHLedger;
+        //竖向斜杆自重
+        private double GVBrace;
+        //水平斜杆自重
+        private double GHBrace;
+
+        private int Step;
+
+       /// <summary>
+        /// 构造函数
+       /// </summary>
+       /// <param name="GStandingTube">KN</param>
+       /// <param name="GVLedger">KN</param>
+       /// <param name="GHLedger">KN</param>
+       /// <param name="GVBrace">KN</param>
+       /// <param name="GHBrace">KN</param>
+       /// <param name="Step">步数</param>
+        
+        public F_NG1K(double GStandingTube, double GVLedger,double GHLedger,double GVBrace,double GHBrace,int Step)
+        {
+            this.GStandingTube = GStandingTube;
+            this.GVLedger = GVLedger;
+            this.GHLedger = GVLedger;
+            this.GVBrace = GVBrace;
+            this.GHBrace = GHBrace;
+            this.Step = Step;
+        }
+
+        public override double ComputeValue()
+        {
+            _targetValue = (GStandingTube + GVLedger + (GHLedger + GVBrace + GHBrace) * 0.5) * Step;
+            _isComputed = true;
+            return _targetValue;
+        }
+
+        public override string ToString()
+        {
+            if (_isComputed)
+            {
+                return "[" + GStandingTube.ToString("#0.0") + "+" + GVLedger.ToString("#0.0") + "+(" + GHLedger.ToString("#0.0") + "+" + GVBrace.ToString("#0.0") + "+" + GHBrace.ToString("#0.0") + ")×0.5]×" + Step.ToString() + "=" + _targetValue.ToString("#0.0");
+            }
+            else
+                return "";
+        }
+
+
+
     }
 }
