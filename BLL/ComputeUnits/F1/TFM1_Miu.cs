6e63f9ef9ae39af741bc67331f9d745109a9fa1f
yayayouyou66 <1141867573@qq.com>
2017-08-31 11:00:58 +0800

    脚手架立杆计算长度系数

--- BLL/ComputeUnits/F1/TFM1_Miu.cs
+++ BLL/ComputeUnits/F1/TFM1_Miu.cs
@@ -2,10 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DAL;
 
 namespace BLL.ComputeUnits.F1
 {
-    class TFM1_Miu
+    //脚手架立杆计算长度系数
+    public class TFM1_Miu:Table<double>
     {
+        string anchor_style;//连墙件布置
+        public TFM1_Miu(string anchor_style)//构造函数
+        {
+            this.anchor_style = anchor_style;
+        }
+        public override double Search()
+        {
+
+            switch (anchor_style)//连墙件布置的一维表查询
+            {
+                case "2步2跨":
+                    {
+                        _targetValue = 1.2;
+                        _isSearched = true;
+                    }
+                    break;
+                case "2步3跨":
+                    {
+                        _targetValue = 1.45;
+                        _isSearched = true;
+                    }
+                    break;
+                case "3步3跨":
+                    {
+                        _targetValue = 1.70;
+                        _isSearched = true;
+                    }
+                    break;
+            }
+            if (_isSearched)
+                return _targetValue;
+            else
+                return 0.0;
+        }
+       
     }
 }
