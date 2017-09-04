119e6e30bbddc8f0fe619c300955162e93aa2aa3
yayayouyou66 <1141867573@qq.com>
2017-09-04 15:07:13 +0800

    f8

---
+++ BLL/ComputeUnits/F8/Controller8.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Model.Entity;
+using BLL.ComputeUnits.F1;
+
+namespace BLL.ComputeUnits.F8
+{
+    public class Controller8
+    {
+        public static F_PCF f_pcf = null;
+        
+        //计算出pk的值
+        private void Calcpk()
+        {
+            double NG1K = Controller1.f_NG1K.TargetValue;
+            double NG2K = Controller1.f_NG2K.TargetValue;
+            double NQK = Controller1.f_NQK.TargetValue;
+            double NK = NG1K + NG2K + NQK;
+            f_pcf = new F_PCF(NG1K, NG2K, NQK, NK,ScaffoldPara.Pad_Area , f_pcf.TargetValue);
+            f_pcf.ComputeValue();
+        }
+        
+        public static string lString = "";
+        public static string rString = "";
+        //比较pk与与fg的大小，符合要求输出不符合抛出异常
+        public void Compare()
+        {
+            if(f_pcf.TargetValue<=ScaffoldPara.Cha_Value)
+            {
+                double fg = ScaffoldPara.Cha_Value;
+                lString = f_pcf.TargetValue.ToString("#0.00");
+                rString = fg.ToString("#0.00");
+            }
+            else
+                throw new Exception("地基承载力验算未通过");
+
+        }
+    }
+}
---
+++ BLL/ComputeUnits/F8/F_PCF.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.ComputeUnits.F8
+{
+    public class F_PCF:Formula<double>
+    {
+        //输出拼凑字符串
+        private double NG1K;
+        private double NG2K; 
+        private double NQK;
+        private double NK;
+        private double Pad_Area;
+        private double pk;
+        public F_PCF(double NG1K, double NG2K, double NQK, double NK, double Pad_Area, double pk)
+        {
+            this.NG1K = NG1K;
+            this.NG2K = NG2K;
+            this.NQK = NQK;
+            this.NK = NK;
+            this.Pad_Area = Pad_Area;
+            this.pk = pk;
+        }
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <returns>kpa</returns>
+        /// //结果要赋给_targetValue和_isComputed标志位置true
+        public override double ComputeValue()
+        {
+            NK = NG1K + NG2K + NQK;
+            pk=NK / Pad_Area;
+            _targetValue = pk;
+            _isComputed = true;
+            return _targetValue;
+        }
+    }
+}
+
